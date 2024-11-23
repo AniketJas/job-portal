cec5737557344eb696714c28269baaccb3d17228
Aniket Jas <aniket.jas20@gmail.com>
2024-11-24 02:38:16 +0530

    Added database and related schema

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import cookieParser from "cookie-parser"
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
+import connectDB from "./utils/db.js"
+
+dotenv.config({})
 
 const app = express()
-const PORT = 9090
+const PORT = process.env.PORT || 9090
 const corsOptions = {
   origin: "http://localhost:5173/",
   credentials: true
@@ -23,5 +27,6 @@ app.post("/test", (req, res) => {
 })
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running at http://localhost:${PORT}`)
 })
---
+++ server/models/company.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  logo: {
+    type: String,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+}, { timestamps: true })
+
+export const Company = mongoose.Schema("Company", companySchema)
