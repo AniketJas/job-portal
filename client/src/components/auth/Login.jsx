ec73245e618cd74c427688eecca497b5bfe46de5
Aniket Jas <aniket.jas20@gmail.com>
2024-12-08 14:57:12 +0530

    Connected Login page with backend

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -3,19 +3,60 @@ import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { RadioGroup } from '../ui/radio-group'
 import { Button } from '../ui/button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
+import { USER_API_END_POINT } from '@/utils/constant'
+import { toast } from 'sonner'
 
 const Login = () => {
+
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  })
+
+  const navigate = useNavigate();
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+        header: {
+          "Content-Type": "application/json"
+        },
+        withCredentials: true
+      })
+
+      if (res.data.success) {
+        navigate('/')
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message)
+    }
+  }
+
   return (
     <div>
       <Navbar />
       <div className='flex items-center justify-center max-w-7xl mx-auto'>
-        <form action="" className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+        <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
           <h1 className='font-bold text-xl mb-5'>Login</h1>
           <div className=''>
             <Label>Email</Label>
             <Input
               type="email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
               placeholder="johndoe@gmail.com"
             />
           </div>
@@ -23,29 +64,40 @@ const Login = () => {
             <Label>Password</Label>
             <Input
               type="password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
               placeholder="*******"
             />
           </div>
           <div className='flex items-center justify-between'>
             <RadioGroup className='flex items-center gap-4 my-5'>
               <div className="flex items-center space-x-2">
-                <Input type='radio' name='role' value="student" className='cursor-pointer' />
+                <Input
+                  type='radio'
+                  name='role'
+                  value="student"
+                  className='cursor-pointer'
+                  checked={input.role === "student"}
+                  onChange={changeEventHandler} />
                 <Label htmlFor="student">Student</Label>
               </div>
               <div className="flex items-center space-x-2">
-                <Input type='radio' name='role' value="recruiter" className='cursor-pointer' />
+                <Input
+                  type='radio'
+                  name='role'
+                  value="recruiter"
+                  className='cursor-pointer'
+                  checked={input.role === "recruiter"}
+                  onChange={changeEventHandler}
+                />
                 <Label htmlFor="recruiter">Recruiter</Label>
               </div>
             </RadioGroup>
-
-            <div className='flex items-center gap-2'>
-              <Label>Profile</Label>
-              <Input accept='image/*' type="file" className="cursor-pointer" />
-            </div>
           </div>
           <Button type="submit" className="w-full my-4">Login</Button>
           <span>
-            Don't have an account ? <Link to="/signup" className='text-blue-600'>Signup</Link>
+            Don&apos;t have an account ? <Link to="/signup" className='text-blue-600'>Signup</Link>
           </span>
         </form>
       </div>
