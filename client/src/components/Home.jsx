62518aa3023e48916272717eabd27a881a79ae37
Aniket Jas <aniket.jas20@gmail.com>
2024-12-11 14:38:06 +0530

    Added Category carousel

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,3 +1,4 @@
+import CategoryCarousel from './CategoryCarousel'
 import HeroSection from './HeroSection'
 import Navbar from './shared/Navbar'
 
@@ -6,8 +7,8 @@ const Home = () => {
     <div>
       <Navbar />
       <HeroSection />
-      {/* <CategoryCarousel />
-      <LatestJobs />
+      <CategoryCarousel />
+      {/*<LatestJobs />
       <Footer /> */}
     </div>
   )
