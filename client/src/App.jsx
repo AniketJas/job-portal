29f156e8a2899425dbddd357fbcc021eba0edb8b
Aniket Jas <aniket.jas20@gmail.com>
2024-12-12 12:26:38 +0530

    Added Browse page with search result

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./components/auth/Login"
 import Signup from "./components/auth/Signup"
 import Home from "./components/Home"
 import Jobs from "./components/Jobs"
+import Browse from "./components/Browse"
 
 const appRoutes = createBrowserRouter([
   {
@@ -21,7 +22,10 @@ const appRoutes = createBrowserRouter([
     path: '/jobs',
     element: <Jobs />
   },
-
+  {
+    path: '/browse',
+    element: <Browse />
+  },
 ])
 
 function App() {
